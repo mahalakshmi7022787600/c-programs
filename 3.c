6d65837d1152fbe4f09b6497b9b07f7c2e90ff2a
mahalakshmi7022787600 <48087377+mahalakshmi7022787600@users.noreply.github.com>
2019-02-28 15:44:07 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+float f;
+printf("enter a number");
+scanf("%f",&f);
+return 0;
+}
---
+++ 6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter an integer:");
+scanf("%d",&number);
+if(number%2==0)
+printf("%d is even intger",number);
+else
+if("%d is an odd integer",number);
+return 0;
+}
